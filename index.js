fe9409e8c01a126f6e1b20bdead46e826bdf04d5
Jasper Moelker <j.b.moelker@activeids.nl>
2018-03-19 16:20:39 +0100

    Display git and env variables

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const git = require('git-rev')
 const { send } = require('micro')
 const { name } = require('./package.json')
+const {
+  NODE_ENV,
+  NOW = false,
+  NOW_ALIAS = "",
+  NOW_URL = "",
+  TRAVIS_BRANCH = "",
+  TRAVIS_PULL_REQUEST = null,
+} = process.env
+
+const getBranch = () => new Promise((resolve) => git.branch(value => resolve(value)))
+const getSha = () => new Promise((resolve) => git.short(value => resolve(value)))
 
 module.exports = async (req, res) => {
-    send(res, 200, {
-        name
-    })
+  const [branch, sha] = await Promise.all([ getBranch(), getSha() ])
+  send(res, 200, { 
+    name, 
+    branch, 
+    sha,
+    NODE_ENV,
+    NOW,
+    NOW_ALIAS,
+    NOW_URL,
+    TRAVIS_BRANCH,
+    TRAVIS_PULL_REQUEST,
+  })
 }
